89fdcdb68a18d41607dc4f1e3c16378c30b71dbb
cesar <cesar.moreno04@uptc.edu.co>
2023-11-20 14:23:38 -0500

    Change the training.py

--- training.py
+++ training.py
@@ -3,28 +3,28 @@ import json
 import pickle
 import numpy as np
 
-import nltk
-from nltk.stem import WordNetLemmatizer #Para pasar las palabras a su forma raíz
-
-#Para crear la red neuronal
+from nltk.stem import WordNetLemmatizer
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer()
-
-intents = json.loads(open('intents.json').read())
-
+# Descarga los recursos necesarios para NLTK
+import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
-nltk.download('omw-1.4')
 
+# Inicializa el lematizador
+lemmatizer = WordNetLemmatizer()
+
+# Carga los datos de intents.json
+intents = json.loads(open('intents.json').read())
+
+# Procesamiento de datos
 words = []
 classes = []
 documents = []
 ignore_letters = ['?', '!', '¿', '.', ',']
 
-#Clasifica los patrones y las categorías
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
@@ -33,33 +33,33 @@ for intent in intents['intents']:
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
 
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
-#Pasa la información a unos y ceros según las palabras presentes en cada categoría para hacer el entrenamiento
+# Creación de datos de entrenamiento
 training = []
-output_empty = [0]*len(classes)
+output_empty = [0] * len(classes)
+
 for document in documents:
-    bag = []
-    word_patterns = document[0]
-    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    bag = [0] * len(words)
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in document[0]]
     for word in words:
-        bag.append(1) if word in word_patterns else bag.append(0)
+        bag[words.index(word)] = 1 if word in word_patterns else 0
+
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
+
 random.shuffle(training)
-training = np.array(training)
-print(training)
 
-#Reparte los datos para pasarlos a la red
-train_x = list(training[:,0])
-train_y = list(training[:,1])
+# Convierte las listas en arrays NumPy
+train_x = np.array([entry[0] for entry in training])
+train_y = np.array([entry[1] for entry in training])
 
-#Creamos la red neuronal
+# Definición del modelo de la red neuronal
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -67,10 +67,12 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-#Creamos el optimizador y lo compilamos
-sgd = SGD(learning_rate=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+# Compilación del modelo
+sgd = SGD(learning_rate=0.001, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#Entrenamos el modelo y lo guardamos
-train_process = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
-model.save("chatbot_model.h5", train_process)
+# Entrenamiento del modelo
+model.fit(train_x, train_y, epochs=100, batch_size=5, verbose=1)
+
+# Guarda el modelo entrenado
+model.save("chatbot_model.h5")
